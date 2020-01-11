8fe3df115b4d82d0bf84f2aa597925ad6c559c7e
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2020-01-11 14:05:35 +0100

    Create Review model

---
+++ FoodUp.Web/Migrations/20200111115958_CreateReviews.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodUp.Web.Migrations
+{
+  public partial class CreateReviews : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.CreateTable(
+          name: "Review",
+          columns: table => new {
+              Id = table.Column<int>(nullable: false)
+                  .Annotation("Sqlite:Autoincrement", true),
+              Rating = table.Column<int>(nullable: false),
+              Comment = table.Column<string>(nullable: false),
+              ReviewerId = table.Column<int>(nullable: false),
+              RecipeId = table.Column<int>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Review", x => x.Id);
+          });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(name: "Review");
+    }
+  }
+}
