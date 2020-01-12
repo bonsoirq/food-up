df7b52003dffedf540fc424e61d60045a81c4159
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2020-01-12 11:13:52 +0100

    Add User test

---
+++ FoodUp.Tests/UserTests.cs
@@ -0,0 +1,26 @@
+using FoodUp.Web.Models;
+using NUnit.Framework;
+
+namespace FoodUp.Tests
+{
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EncryptsPasswordAndRemovesPlainPasswordProperty()
+        {
+            var user = new User()
+            {
+                Password = "password"
+            };
+
+            user.EncryptPassword();
+            Assert.IsNull(user.Password);
+            Assert.IsNotNull(user.EncryptedPassword);
+        }
+    }
+}
