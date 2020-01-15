7890f33a4279aece496bc6c74f3614f84e2cfb21
Maksymilian Galas <maksg@onet.eu>
2020-01-15 20:21:24 +0000

    Create Ingredient model

---
+++ FoodUp.Web/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodUp.Web.Models
+{
+    public class Ingredient
+    {
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; }
+
+        public string GetIngredientFullDescription()
+        {
+            return $"{Name} - {Quantity} {Unit}";
+        }
+    }
+}
