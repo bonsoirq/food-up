be6ef5d1c5d69c09e6e54db676ff933ed4481952
Maksymilian Galas <maksg@onet.eu>
2020-01-16 13:27:19 +0000

    Add Ingredient tests

---
+++ FoodUp.Tests/IngredientTests.cs
@@ -0,0 +1,31 @@
+﻿using FoodUp.Web.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodUp.Tests
+{
+    public class IngredientTests
+    {
+        private Ingredient ingredient;
+
+        [SetUp]
+        public void Setup()
+        {
+            ingredient = new Ingredient();
+            ingredient.Id = 1;
+            ingredient.Name = "Bananas";
+            ingredient.Quantity = 123;
+            ingredient.Unit = "pcs";
+
+        }
+
+        [Test]
+        public void IngredientFullDescriptionIsNotNull()
+        {
+            string result = ingredient.GetIngredientFullDescription();
+            Assert.IsNotNull(result);
+        }
+    }
+}
