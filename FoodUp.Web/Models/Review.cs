73ac3c9e487d28ccb85154510a5c700e868b10a1
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2020-02-01 16:48:19 +0100

    Allow users to add only one review per recipe

--- FoodUp.Web/Models/Review.cs
+++ FoodUp.Web/Models/Review.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FoodUp.Web.Models
 {
     public class Review
     {
         public int Id { get; set; }
+        [Required]
         public int Rating { get; set; }
+        [Required]
         public string Comment { get; set; }
         public int ReviewerId { get; set; }
         public int RecipeId { get; set; }
--- FoodUp.Web/Models/User.cs
+++ FoodUp.Web/Models/User.cs
@@ -7,11 +7,18 @@ namespace FoodUp.Web.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(15)]
         public string Login { get; set; }
         [NotMapped]
+        [Required]
+        [MinLength(8)]
+        [MaxLength(32)]
         public string Password { get; set; }
         public string EncryptedPassword { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
 
